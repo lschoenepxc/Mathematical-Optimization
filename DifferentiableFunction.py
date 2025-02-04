31c65a3426610c6594b3125eb0d8e9263fc279d0
LauraSchöne <schoene_l@web.de>
2025-02-04 13:59:00 +0100

    working functions classes

--- DifferentiableFunction.py
+++ DifferentiableFunction.py
@@ -3,31 +3,33 @@ from Set import ISet, AffineSpace
 import numpy as np
 from typing import Callable, Union
 from multimethod import multimethod
+from Function import IFunction, Function
 
 
-class IDifferentiableFunction(object):
+class IDifferentiableFunction(IFunction):
     """This interface models differentiable Functions from ISet to R^n."""
 
     def __init__(self, name: str, domain: ISet):
-        super().__init__()
-        self._name = name
-        self._domain = domain
+        # super().__init__()
+        # self._name = name
+        # self._domain = domain
+        super().__init__(name=name, domain=domain)
 
-    @abstractmethod
-    def evaluate(self, point: np.ndarray) -> np.ndarray:
-        """Evaluates the function at point.
-        The parameter "point" is a vector in Double^(Domain.ambient_dimension) such that Domain.is_contained(point)=true."""
-        pass
+    # @abstractmethod
+    # def evaluate(self, point: np.ndarray) -> np.ndarray:
+    #     """Evaluates the function at point.
+    #     The parameter "point" is a vector in Double^(Domain.ambient_dimension) such that Domain.is_contained(point)=true."""
+    #     pass
 
-    @property
-    def domain(self) -> ISet:
-        """The domain of the function, i.e., the set of points at which the function can be evaluated."""
-        return self._domain
+    # @property
+    # def domain(self) -> ISet:
+    #     """The domain of the function, i.e., the set of points at which the function can be evaluated."""
+    #     return self._domain
 
-    @property
-    def name(self) -> str:
-        """The name of the function, might be used for debugging"""
-        return self._name
+    # @property
+    # def name(self) -> str:
+    #     """The name of the function, might be used for debugging"""
+    #     return self._name
 
     @abstractmethod
     def jacobian(self, point: np.ndarray) -> np.ndarray:
@@ -38,64 +40,103 @@ class IDifferentiableFunction(object):
     @multimethod
     def __add__(self, other: Union[int, float]) -> 'IDifferentiableFunction':
         """Adds the two functions value wise, where the second function is a constant"""
+        added_function = Function.__add__(self, other)
         return DifferentiableFunction(
-            name="(" + self.name + ") + " + str(other),
-            domain=self.domain,
-            evaluate=lambda v: self.evaluate(v) + other,
+            name=added_function.name,
+            domain=added_function.domain,
+            evaluate=added_function.evaluate,
             jacobian=lambda v: self.jacobian(v)
         )
+        # return DifferentiableFunction(
+        #     name="(" + self.name + ") + " + str(other),
+        #     domain=self.domain,
+        #     evaluate=lambda v: self.evaluate(v) + other,
+        #     jacobian=lambda v: self.jacobian(v)
+        # )
 
     @multimethod
     def __add__(self, other: 'IDifferentiableFunction') -> 'IDifferentiableFunction':
         """Adds the two functions value wise"""
+        added_function = Function.__add__(self, other)
         return DifferentiableFunction(
-            name="(" + self.name + ") + (" + other.name + ")",
-            domain=self.domain.intersect(other.domain),
-            evaluate=lambda v: self.evaluate(v) + other.evaluate(v),
+            name=added_function.name,
+            domain=added_function.domain,
+            evaluate=added_function.evaluate,
             jacobian=lambda v: self.jacobian(v) + other.jacobian(v)
         )
+        # return DifferentiableFunction(
+        #     name="(" + self.name + ") + (" + other.name + ")",
+        #     domain=self.domain.intersect(other.domain),
+        #     evaluate=lambda v: self.evaluate(v) + other.evaluate(v),
+        #     jacobian=lambda v: self.jacobian(v) + other.jacobian(v)
+        # )
 
     def __mul__(self, other: Union[int, float]) -> 'IDifferentiableFunction':
         """Multiplies the function by a scalar"""
+        multiplied_function = Function.__mul__(self, other)
         return DifferentiableFunction(
-            name=str(other) + " * (" + self.name + ")",
-            domain=self.domain,
-            evaluate=lambda v: other * self.evaluate(v),
+            name=multiplied_function.name,
+            domain=multiplied_function.domain,
+            evaluate=multiplied_function.evaluate,
             jacobian=lambda v: other * self.jacobian(v)
         )
+        # return DifferentiableFunction(
+        #     name=str(other) + " * (" + self.name + ")",
+        #     domain=self.domain,
+        #     evaluate=lambda v: other * self.evaluate(v),
+        #     jacobian=lambda v: other * self.jacobian(v)
+        # )
 
     def __pow__(self, power: int) -> 'IDifferentiableFunction':
         """Take integer exponents of a function"""
+        powered_function = Function.__pow__(self, power)
         return DifferentiableFunction(
-            name="(" + self.name + ")^" + str(power),
-            domain=self.domain,
-            evaluate=lambda v: self.evaluate(v)**power,
+            name=powered_function.name,
+            domain=powered_function.domain,
+            evaluate=powered_function.evaluate,
             jacobian=lambda v: np.matmul(
                 np.array([[power]])*self.evaluate(v)**(power-1), self.jacobian(v))
         )
+        # return DifferentiableFunction(
+        #     name="(" + self.name + ")^" + str(power),
+        #     domain=self.domain,
+        #     evaluate=lambda v: self.evaluate(v)**power,
+        #     jacobian=lambda v: np.matmul(
+        #         np.array([[power]])*self.evaluate(v)**(power-1), self.jacobian(v))
+        # )
 
     def __rmul__(self, other: Union[int, float]):
         """Multiplies the function by a scalar"""
         return self.__mul__(other)
 
+    # warum funktioniert das ohne eine neue Funktion zu erstellen?
     def __sub__(self, other: 'IDifferentiableFunction') -> 'IDifferentiableFunction':
         """Subtracts two functions value wise"""
         return self + (-1) * other
 
     def Pairing(self, other: 'IDifferentiableFunction') -> 'IDifferentiableFunction':
         """Returns the pairing of two functions"""
+        paired_function = Function.Pairing(self, other)
         return DifferentiableFunction(
-            name="Pair(" + self.name + "," + other.name + ")",
-            domain=self.domain.intersect(other.domain),
-            evaluate=lambda v: np.concatenate(
-                (self.evaluate(v), other.evaluate(v))),
+            name=paired_function.name,
+            domain=paired_function.domain,
+            evaluate=paired_function.evaluate,
             jacobian=lambda v: np.concatenate(
                 (self.jacobian(v), other.jacobian(v)))
         )
+        # return DifferentiableFunction(
+        #     name="Pair(" + self.name + "," + other.name + ")",
+        #     domain=self.domain.intersect(other.domain),
+        #     evaluate=lambda v: np.concatenate(
+        #         (self.evaluate(v), other.evaluate(v))),
+        #     jacobian=lambda v: np.concatenate(
+        #         (self.jacobian(v), other.jacobian(v)))
+        # )
 
     def CartesianProduct(self, other: 'IDifferentiableFunction') -> 'IDifferentiableFunction':
         """Returns the cartesian product of two functions"""
 
+        composed_function = Function.CartesianProduct(self, other)
         proj_domain = AffineSpace(
             self.domain._ambient_dimension+other.domain._ambient_dimension)
         proj1 = DifferentiableFunction.Projection(
@@ -107,27 +148,34 @@ class IDifferentiableFunction(object):
         f2 = DifferentiableFunction.FromComposition(
             other, proj2)
         return DifferentiableFunction(
-            name="CartesianProduct(" + self.name + "," + other.name + ")",
-            domain=self.domain.cartesian_product(other.domain),
-            evaluate=lambda v: np.concatenate(
-                (f1.evaluate(v), f2.evaluate(v))),
+            name=composed_function.name,
+            domain=composed_function.domain,
+            evaluate=composed_function.evaluate,
             jacobian=lambda v: np.concatenate(
-                (f1.jacobian(v), f2.jacobian(v)), axis=0)
+                (f1.jacobian(v), f2.jacobian(v)))
         )
+        # return DifferentiableFunction(
+        #     name="CartesianProduct(" + self.name + "," + other.name + ")",
+        #     domain=self.domain.cartesian_product(other.domain),
+        #     evaluate=lambda v: np.concatenate(
+        #         (f1.evaluate(v), f2.evaluate(v))),
+        #     jacobian=lambda v: np.concatenate(
+        #         (f1.jacobian(v), f2.jacobian(v)), axis=0)
+        # )
 
 
-class DifferentiableFunction(IDifferentiableFunction):
+class DifferentiableFunction(Function, IDifferentiableFunction):
     """This class models differentiable Functions from ISet to R^n, where the function and the Jacobian are given by lambdas."""
 
     def __init__(self, name: str, domain: ISet, evaluate: Callable[[np.ndarray], np.ndarray], jacobian: Callable[[np.ndarray], np.ndarray]):
         """ Construct a function from lambdas, for the function itself and for its derivatives
         Sadly, the type system is not strict enough to check sizes of tensors"""
-        super().__init__(name=name, domain=domain)
-        self._evaluate = evaluate
+        super().__init__(name=name, domain=domain, evaluate=evaluate)
+        # self._evaluate = evaluate
         self._jacobian = jacobian
 
-    def evaluate(self, point: np.ndarray) -> np.ndarray:
-        return self._evaluate(point)
+    # def evaluate(self, point: np.ndarray) -> np.ndarray:
+    #     return self._evaluate(point)
 
     def jacobian(self, point: np.ndarray) -> np.ndarray:
         return self._jacobian(point)
@@ -135,52 +183,90 @@ class DifferentiableFunction(IDifferentiableFunction):
     @ classmethod
     def FromComposition(cls, f: IDifferentiableFunction, g: IDifferentiableFunction) -> IDifferentiableFunction:
         """Constructs f ° g"""
+        composed_function = Function.FromComposition(f, g)
         return cls(
-            name="(" + f.name + ") ° (" + g.name + ")",
-            domain=g.domain,
-            evaluate=lambda v: f.evaluate(g.evaluate(v)),
+            name=composed_function.name,
+            domain=composed_function.domain,
+            evaluate=composed_function.evaluate,
             jacobian=lambda v: np.matmul(
                 f.jacobian(g.evaluate(v)), g.jacobian(v))
         )
+        # return cls(
+        #     name="(" + f.name + ") ° (" + g.name + ")",
+        #     domain=g.domain,
+        #     evaluate=lambda v: f.evaluate(g.evaluate(v)),
+        #     jacobian=lambda v: np.matmul(
+        #         f.jacobian(g.evaluate(v)), g.jacobian(v))
+        # )
 
     @ classmethod
     def LinearMapFromMatrix(cls, A: np.array) -> IDifferentiableFunction:
         """Constructs x -> A*x"""
+        linear_function = Function.LinearMapFromMatrix(A)
         return cls(
-            name="linear",
-            domain=AffineSpace(A.shape[1]),
-            evaluate=lambda x: np.matmul(A, x),
+            name=linear_function.name,
+            domain=linear_function.domain,
+            evaluate=linear_function.evaluate,
             jacobian=lambda x: A
         )
+        # return cls(
+        #     name="linear",
+        #     domain=AffineSpace(A.shape[1]),
+        #     evaluate=lambda x: np.matmul(A, x),
+        #     jacobian=lambda x: A
+        # )
 
     @classmethod
     def TranslationByVector(cls, v: np.array) -> IDifferentiableFunction:
         """Constructs x -> x+v"""
         n = v.shape[0]
-        return cls(name="translation", domain=AffineSpace(n), evaluate=lambda x: x+v, jacobian=lambda x: np.eye(n))
+        translated_function = Function.TranslationByVector(v)
+        return cls(
+            name=translated_function.name,
+            domain=translated_function.domain,
+            evaluate=translated_function.evaluate,
+            jacobian=lambda x: np.eye(n)
+        )
+        # return cls(name="translation", domain=AffineSpace(n), evaluate=lambda x: x+v, jacobian=lambda x: np.eye(n))
 
-    def __create_matrix_with_ones(rows: int, columns: int, ones_positions: list[tuple[int, int]]):
-        matrix = np.zeros((rows, columns))
-        row_indices, column_indices = zip(*ones_positions)
-        matrix[row_indices, column_indices] = 1
-        return matrix
+    # def __create_matrix_with_ones(rows: int, columns: int, ones_positions: list[tuple[int, int]]):
+    #     matrix = np.zeros((rows, columns))
+    #     row_indices, column_indices = zip(*ones_positions)
+    #     matrix[row_indices, column_indices] = 1
+    #     return matrix
 
     @ classmethod
     def Projection(cls, domain: ISet, l: list[int]) -> IDifferentiableFunction:
+        """Constructs a projection function"""
+        projected_function = Function.Projection(domain, l)
         return cls(
-            name="projection("+str(l)+")",
-            domain=domain,
-            evaluate=lambda x: x[l],
-            jacobian=lambda x: cls.__create_matrix_with_ones(rows=len(l), columns=domain._ambient_dimension, ones_positions=list(zip(range(len(l)), l))))
+            name=projected_function.name,
+            domain=projected_function.domain,
+            evaluate=projected_function.evaluate,
+            jacobian=lambda x: cls.__create_matrix_with_ones(rows=len(l), columns=domain._ambient_dimension, ones_positions=list(zip(range(len(l)), l)))
+        )
+        # return cls(
+        #     name="projection("+str(l)+")",
+        #     domain=domain,
+        #     evaluate=lambda x: x[l],
+        #     jacobian=lambda x: cls.__create_matrix_with_ones(rows=len(l), columns=domain._ambient_dimension, ones_positions=list(zip(range(len(l)), l)))
+        # )
 
     @classmethod
     def Identity(cls, domain: ISet) -> IDifferentiableFunction:
         n = domain._ambient_dimension
+        identity_function = Function.Identity(domain)
         return cls(
-            name="Id(n)",
-            domain=domain,
-            evaluate=lambda x: x,
-            jacobian=lambda x: np.eye(n))
+            name=identity_function.name,
+            domain=identity_function.domain,
+            evaluate=identity_function.evaluate,
+            jacobian=lambda x: np.eye(n)
+        )
+        # return cls(
+        #     name="Id(n)",
+        #     domain=domain,
+        #     evaluate=lambda x: x,
+        #     jacobian=lambda x: np.eye(n))
 
     @classmethod
     def ReLU(cls, dimension: int) -> IDifferentiableFunction:
--- Function.py
+++ Function.py
@@ -4,16 +4,12 @@ import numpy as np
 from typing import Callable, Union
 from multimethod import multimethod
 
-
 class IFunction(object):
-    """This interface models Functions from ISet to R^n."""
-
-    def __init__(self, name: str, domain: ISet, output_domain: ISet):
+    """This interface models differentiable Functions from ISet to R^n."""
+    def __init__(self, name: str, domain: ISet):
         super().__init__()
         self._name = name
         self._domain = domain
-        self._output_domain = output_domain
-        self._verified = False
 
     @abstractmethod
     def evaluate(self, point: np.ndarray) -> np.ndarray:
@@ -31,11 +27,6 @@ class IFunction(object):
         """The name of the function, might be used for debugging"""
         return self._name
     
-    @property
-    def output_domain(self) -> ISet:
-        """The domain of the function, i.e., the set of points at which the function can be evaluated."""
-        return self._output_domain
-
     @multimethod
     def __add__(self, other: Union[int, float]) -> 'IFunction':
         """Adds the two functions value wise, where the second function is a constant"""
@@ -44,7 +35,7 @@ class IFunction(object):
             domain=self.domain,
             evaluate=lambda v: self.evaluate(v) + other
         )
-
+    
     @multimethod
     def __add__(self, other: 'IFunction') -> 'IFunction':
         """Adds the two functions value wise"""
@@ -54,12 +45,6 @@ class IFunction(object):
             evaluate=lambda v: self.evaluate(v) + other.evaluate(v)
         )
         
-    def verify(self):
-        """Verifies that the function is well defined"""
-        if not self._verfiied:
-            assert self._output_domain.shape == self.evaluate(self.domain.point()).shape , "Function is not well defined"
-            self._verfiied = True
-
     def __mul__(self, other: Union[int, float]) -> 'IFunction':
         """Multiplies the function by a scalar"""
         return Function(
@@ -112,20 +97,19 @@ class IFunction(object):
             evaluate=lambda v: np.concatenate(
                 (f1.evaluate(v), f2.evaluate(v)))
         )
-
-
+    
 class Function(IFunction):
-    """This class models Functions from ISet to R^n, where the function is given by lambdas."""
+    """This class models differentiable Functions from ISet to R^n, where the function and the Jacobian are given by lambdas."""
 
     def __init__(self, name: str, domain: ISet, evaluate: Callable[[np.ndarray], np.ndarray]):
-        """ Construct a function from lambdas, for the function itself
+        """ Construct a function from lambdas, for the function itself and for its derivatives
         Sadly, the type system is not strict enough to check sizes of tensors"""
         super().__init__(name=name, domain=domain)
         self._evaluate = evaluate
 
     def evaluate(self, point: np.ndarray) -> np.ndarray:
         return self._evaluate(point)
-
+    
     @ classmethod
     def FromComposition(cls, f: IFunction, g: IFunction) -> IFunction:
         """Constructs f ° g"""
@@ -134,7 +118,7 @@ class Function(IFunction):
             domain=g.domain,
             evaluate=lambda v: f.evaluate(g.evaluate(v))
         )
-
+        
     @ classmethod
     def LinearMapFromMatrix(cls, A: np.array) -> IFunction:
         """Constructs x -> A*x"""
@@ -143,110 +127,36 @@ class Function(IFunction):
             domain=AffineSpace(A.shape[1]),
             evaluate=lambda x: np.matmul(A, x)
         )
-
+        
     @classmethod
     def TranslationByVector(cls, v: np.array) -> IFunction:
         """Constructs x -> x+v"""
         n = v.shape[0]
         return cls(name="translation", domain=AffineSpace(n), evaluate=lambda x: x+v)
-
+    
     def __create_matrix_with_ones(rows: int, columns: int, ones_positions: list[tuple[int, int]]):
         matrix = np.zeros((rows, columns))
         row_indices, column_indices = zip(*ones_positions)
         matrix[row_indices, column_indices] = 1
         return matrix
-
+    
     @ classmethod
     def Projection(cls, domain: ISet, l: list[int]) -> IFunction:
         return cls(
             name="projection("+str(l)+")",
             domain=domain,
             evaluate=lambda x: x[l]
-            )
-
+        )
+    
     @classmethod
     def Identity(cls, domain: ISet) -> IFunction:
         n = domain._ambient_dimension
         return cls(
             name="Id(n)",
             domain=domain,
-            evaluate=lambda x: x)
-
-    @classmethod
-    def ReLU(cls, dimension: int) -> IFunction:
-        """Returns a ReLU function"""
-        return cls(name="ReLU", domain=AffineSpace(dimension), evaluate=lambda x: np.maximum(0, x))
+            evaluate=lambda x: x
+        )
     
-    ### Aufgabe 4.2: Implemetiere 10 weitere Funktionen: sin, cos, tan, exp, log, sqrt, sigmoid, heaviside, square, cube
-    @classmethod
-    def sin(cls, dimension: int) -> IFunction:
-        """Returns a sinus function"""
-        return cls(name="sinus", domain=AffineSpace(dimension), evaluate=lambda x: np.sin(x))
-    
-    @classmethod
-    def cos(cls, dimension: int) -> IFunction:
-        """Returns a cosinus function"""
-        return cls(name="cosinus", domain=AffineSpace(dimension), evaluate=lambda x: np.cos(x))
-    
-    @classmethod
-    def tan(cls, dimension: int) -> IFunction:
-        """Returns a tan function"""
-        return cls(name="tan", domain=AffineSpace(dimension), evaluate=lambda x: np.tan(x))
-    
-    @classmethod
-    def exp(cls, dimension: int) -> IFunction:
-        """Returns a exponential function"""
-        return cls(name="exp", domain=AffineSpace(dimension), evaluate=lambda x: np.exp(x))
-    
-    @classmethod
-    def log(cls, dimension: int) -> IFunction:
-        """Returns a logarithm function"""
-        return cls(name="log", domain=AffineSpace(dimension), evaluate=lambda x: np.log(x))
-    
-    @classmethod
-    def sqrt(cls, dimension: int) -> IFunction:
-        """Returns a square root function"""
-        return cls(name="sqrt", domain=AffineSpace(dimension), evaluate=lambda x: np.sqrt(x))
-    
-    @classmethod
-    def sigmoid(cls, dimension: int) -> IFunction:
-        """Returns a sigmoid function"""
-        return cls(name="sigmoid", domain=AffineSpace(dimension), evaluate=lambda x: 1/(1+np.exp(-x)))
-    
-    @classmethod
-    def heaviside(cls, dimension: int) -> IFunction:
-        """Returns a heaviside function"""
-        return cls(name="heaviside", domain=AffineSpace(dimension), evaluate=lambda x: 1*(x>0))
-    
-    @classmethod
-    def square(cls, dimension: int) -> IFunction:
-        """Returns a square function"""
-        return cls(name="square", domain=AffineSpace(dimension), evaluate=lambda x: x**2)
-    
-    @classmethod
-    def cube(cls, dimension: int) -> IFunction:
-        """Returns a cube function"""
-        return cls(name="cube", domain=AffineSpace(dimension), evaluate=lambda x: x**3)
-    
-    ### Ende Aufgabe 4.2
-    
-    ### Aufgabe 4.3: Implementiere f(x) = (sqrt(cube(x)+2*square(x)-x+1)*exp(sin(square(x))))/(log(square(square(x))+2)+arccos(x/2))
-    
-    @classmethod
-    def arccos(cls, dimension: int) -> IFunction:
-        """Returns a arccos function"""
-        return cls(name="arccos", domain=AffineSpace(dimension), evaluate=lambda x: np.arccos(x))
-    
-    @classmethod
-    def own_function(cls, dimension: int) -> IFunction:
-        """Returns a own function"""
-        return cls(name="own_function", domain=AffineSpace(dimension), evaluate=lambda x: (np.sqrt(x**3+2*x**2-x+1)*np.exp(np.sin(x**2)))/(np.log(x**4+2)+np.arccos(x/2)))
-
-    @classmethod
-    def TwoNormSquared(cls, dimension: int) -> IFunction:
-        """Returns a function computing the squared two norm of a vector"""
-        return cls(name="TwoNormSquared", domain=AffineSpace(dimension), evaluate=lambda x: np.linalg.norm(x)**2)
-
     @classmethod
     def Debug(cls, f: IFunction) -> IFunction:
         """Returns a modified function that prints its inputs and outputs"""
